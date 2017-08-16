1fed8433eb5930d4191db5d9b18f7bcd5438bcb1
Erik McComb <erik@erikmccomb.com>
2017-08-16 11:34:05 -0600

    completed git command help program

--- main.rb
+++ main.rb
@@ -1,6 +1,23 @@
-puts 'Learning Git'
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+end
 
-first_name = 'Erik'
-last_name = 'McComb'
+def menu
+  puts '1: Enter a Git command.'
+  puts '2: exit.'
+  choice = gets.to_i
+  case choice
+    when 1
+      puts 'Enter a Git command'
+      puts_git(gets.strip)
+    when 2
+      puts 'Thanks for using the git command'
+      exit
+    else
+      puts 'Invalid choice!'
+      menu
+  end
+  menu
+end
 
-puts "Hello #{first_name} #{last_name}"
+menu

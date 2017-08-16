aca72972b7ad52e319508ad631b5c7096c830ace
Erik McComb <erik@erikmccomb.com>
2017-08-16 12:07:51 -0600

    added colorize and pry

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
+require 'colorize'
+require 'pry'
+
 def puts_git(cmd)
   puts `git #{cmd} -h`
 end
 
 def menu
+  puts 'Main Menu'.colorize(:light_blue)
   puts '1: Enter a Git command.'
-  puts '2: exit.'
+  puts '2: exit.'.colorize(:red)
   choice = gets.to_i
   case choice
     when 1
